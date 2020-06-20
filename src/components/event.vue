67b2dfde52d3e93622ef6a20bb5a1957d2a09560
went <872701869@qq.com>
2020-06-20 21:05:05 +0800

    blank

---
+++ src/components/event.vue
@@ -0,0 +1,23 @@
+<template>
+    <div @click="handleAdd(2)">
+        点击次数：{{ counter }}
+        <button @click.stop="handleAdd()"> + 1</button>
+        <button @click="handleAdd(10)"> + 10</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Event',
+    desc: '5.4方法与事件',
+    data(){
+        return {counter : 0}
+    },
+    methods:{
+        handleAdd: function(count){
+            count = count || 1;
+            this.counter += count;
+        }
+    }
+}
+</script>
